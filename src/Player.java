1b8b75edc0acd4f569f0f3c764d1da79f008f211
Enba417 <ben_carr_white@hotmail.com>
2016-01-24 23:40:51 -0800

    finished iteration one

--- src/Player.java
+++ src/Player.java
@@ -1,27 +1,102 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Timer;
+
 public class Player {
 	long time;
 	public Tree gameTree = null;
-	public Player(Tree tree) {gameTree=tree;}
-	public Boolean play(ArrayList<Boolean> moves) {
-		
+
+	public Player(Tree tree) {
+		gameTree = tree;
+	}
+
+	private ArrayList<pointValueStruct> pvsList;
+
+	public Boolean play(ArrayList<Boolean> moves) throws InterruptedException {
+
+		pointValueStruct max = new pointValueStruct(-99999,-99999,moves);
 		time = System.currentTimeMillis();
-		return null;
-		
-		
+		(new good(new pointValueStruct(0,0,moves))).start();
+		this.wait(0, 26000);
+		for (pointValueStruct pvs : pvsList)
+			if (pvs.Alpha + pvs.Beta > max.Alpha + max.Beta)
+				max = pvs;
+		return max.moves.get(moves.size());
+
 	}
-	private  pointValueStruct good(pointValueStruct p) {
-		float truefloat = gameTree.value(p.moves.add(true));
-		float falsefloat = gameTree.value(p.moves.add(false));
-		return null;
-				
+
+	private class good extends Thread {
+
+		private pointValueStruct pvs;
+
+		public good(pointValueStruct p) {
+			pvs = p;
+		}
+
+		public void run() {
+
+			if (System.currentTimeMillis() - time >= 25000 || pvs.moves.size() >= gameTree.height) {
+				pvsList.add(pvs);
+				return;
+			} else {
+				pvs.moves.add(true);
+				float truefloat = gameTree.value(pvs.moves);
+				if (pvs.Alpha + truefloat + pvs.Beta >=0) {
+					pvs.Alpha += truefloat;
+					(new bad(pvs)).start();
+					pvs.Alpha -= truefloat;
+				}
+				pvs.moves.remove(pvs.moves.size() - 1);
+				pvs.moves.add(false);
+				float falsefloat = gameTree.value(pvs.moves);
+				if (pvs.Alpha + falsefloat + pvs.Beta >= 0) {
+					pvs.Alpha += falsefloat;
+					(new bad(pvs)).start();
+				}
+			}
+			return;
+		
+
+		}
 	}
+
+	private class bad extends Thread {
+		private pointValueStruct pvs;
+		
+		public bad(pointValueStruct p) {
+			pvs = p;
+		}
+
+		public void run() {
+			if (System.currentTimeMillis() - time >= 25000 || pvs.moves.size() >= gameTree.height) {
+				pvsList.add(pvs);
+				return;
+			} else {
+				pvs.moves.add(true);
+				float truefloat = gameTree.value(pvs.moves);
+				pvs.moves.remove(pvs.moves.size() - 1);
+				pvs.moves.add(false);
+				float falsefloat = gameTree.value(pvs.moves);
+				pvs.moves.remove(pvs.moves.size() - 1);
+				if (truefloat < falsefloat) {
+					pvs.moves.add(true);
+					pvs.Beta += truefloat;
+				} else {
+					pvs.moves.add(false);
+					pvs.Beta += falsefloat;
+				}
+			}
+			return;
+		}
+	}
+	
+
 	private class pointValueStruct {
 		public float Alpha;
 		public float Beta;
 		public ArrayList<Boolean> moves;
-		public pointValueStruct(float A, float B,ArrayList<Boolean> m) {
+
+		public pointValueStruct(float A, float B, ArrayList<Boolean> m) {
 			Alpha = A;
 			Beta = B;
 			moves = m;
--- src/Tree.java
+++ src/Tree.java
@@ -1,4 +1,64 @@
 
-public class Tree {
+import java.util.ArrayList;
+import java.util.Random;
 
+public class Tree {
+	public final int TOTALNODES=2097152;
+	public final float EPSILON=0.00001f;
+	public final float distribution=10.0f;
+	
+	public long randomSeed=0L;
+	public float[] topTree=new float[TOTALNODES];
+	private Random r=new Random();
+	public long height=40;
+	
+	public Tree(long rs) {
+		randomSeed=rs;
+		r.setSeed(randomSeed);
+		topTree[0]=topTree[1]=0.0f;
+		int current=2; int levelNodes=2; float factor=1.0f;
+		while (current<TOTALNODES) {
+			for (int i=0; i<levelNodes; i++) {
+				int parent=current/2;
+				float sign=0.0f;
+				if (r.nextBoolean()&&r.nextBoolean())
+					if (topTree[parent]>EPSILON) sign=1.0f; else sign=-1.0f;
+				else if (topTree[parent]>EPSILON) sign=-1.0f; else sign=1.0f;
+				float offset=((Math.abs(topTree[parent])<EPSILON)?(r.nextFloat()*2.0f*distribution-distribution):(r.nextFloat()*distribution*factor*sign));
+				topTree[current]=topTree[parent]+offset;
+				current++; }
+			levelNodes*=2; factor*=0.9f; }
+	}
+	
+	public float value(ArrayList<Boolean> moves) {
+		// low and high will both be values between 0 and 2^21-1=TOTALNODES.
+		// The depth will be the number of bits examined, starting with the low order bit of the low int.
+		// A depth of 0 will indicate the root.
+		int position=1;
+		for (int i=0; i<Math.min(20, moves.size()); i++) {
+			if (moves.get(i).booleanValue()) position=position*2+1; else position*=2; }
+		if (moves.size()<=20) return topTree[position];
+		r.setSeed(randomSeed+position);
+		
+		float[] bottomTree=new float[TOTALNODES];
+		bottomTree[0]=bottomTree[1]=topTree[position];
+		int current=2; int levelNodes=2; float factor=0.12157665459056928801f;
+		while (current<TOTALNODES) {
+			for (int i=0; i<levelNodes; i++) {
+				int parent=current/2;
+				float sign=0.0f;
+				if (r.nextBoolean()&&r.nextBoolean())
+					if (bottomTree[parent]>EPSILON) sign=1.0f; else sign=-1.0f;
+				else if (bottomTree[parent]>EPSILON) sign=-1.0f; else sign=1.0f;
+				float offset=((Math.abs(bottomTree[parent])<EPSILON)?(r.nextFloat()*2.0f*distribution-distribution):(r.nextFloat()*distribution*factor*sign));
+				bottomTree[current]=bottomTree[parent]+offset;
+				current++; }
+			levelNodes*=2; factor*=0.9f; }
+		
+		position=1;
+		for (int i=20; i<moves.size(); i++) {
+			if (moves.get(i).booleanValue()) position=position*2+1; else position*=2; }
+		return bottomTree[position];
+	}
 }
+
